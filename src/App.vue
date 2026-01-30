db058de8e6183b50c4a842fc874334b5abc9c7e6
chucrutes <tmelo387@gmail.com>
2026-01-29 22:25:00 -0300

    feat: started sign in form

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
-<template>
-  <div class="flex flex-col min-h-screen">
-    <header class="sticky top-0 bg-primary z-50">
-      <div class="flex items-center px-6 h-32 relative w-full">
-        <div class="flex flex-1 justitfy-start">
-          <img src="/logo.png" alt="Logo" width="35" />
-        </div>
-        <nav class="flex-none">
-          <ul class="flex flex-row">
-            <Li>Item 1</Li>
-            <Li>Item 2</Li>
-            <Li>Item 3</Li>
-          </ul>
-        </nav>
-      </div>
-      <div class="flex-1"></div>
-    </header>
-    <main class="flex-1 bg-primary p-4">
-      <p class="text-left">teste</p>
-    </main>
-    <footer class="h-32">
-      <h1>Footer area</h1>
-    </footer>
-  </div>
-</template>
-
 <script setup lang="ts">
-import Li from "./components/atoms/li.vue";
+import { ref } from "vue";
+import Input from "./components/atoms/input.vue";
+
+const email = ref("");
+const password = ref("");
 </script>
+
+<template>
+  <main>
+    <form action="">
+      <label for="email">
+        Email:
+        <Input
+          v-model="password"
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+        />
+      </label>
+      <label for="password">
+        password:
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          v-model="email"
+        />
+      </label>
+    </form>
+  </main>
+</template>
