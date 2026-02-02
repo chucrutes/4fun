ace65a3c7eead452001154167f90b8fbef044268
chucrutes <tmelo387@gmail.com>
2026-02-02 20:02:21 -0300

    fix: moved auth route check to auth guard file

---
+++ src/guards/auth-guard.ts
@@ -0,0 +1,31 @@
+import { useAuthStore } from "@/stores/auth";
+import type { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
+
+export async function authGuard(
+  to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) {
+  const authStore = useAuthStore();
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const isUserAuthenticated = authStore.isAuthenticated;
+
+  if (requiresAuth && !isUserAuthenticated) {
+    return next("/");
+  }
+
+  if (isUserAuthenticated) {
+    try {
+      await authStore.fetchUser();
+    } catch (error) {
+      authStore.logout();
+      return next("/");
+    }
+  }
+
+  if (to.path === "/" && isUserAuthenticated) {
+    return next("/dashboard");
+  }
+
+  next();
+}
