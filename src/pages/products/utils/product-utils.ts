a28f4ddef598918b18972e4a4e749c9ea0af17d6
chucrutes <tmelo387@gmail.com>
2026-02-02 12:28:43 -0300

    fix: removed unused function

--- src/pages/products/utils/product-utils.ts
+++ src/pages/products/utils/product-utils.ts
@@ -4,14 +4,4 @@ export class ProductUtils {
   static highestPriceProducts(products: Product[], quantity: number) {
     return products.sort((a, b) => b.price - a.price).slice(0, quantity);
   }
-  static test(products: Product[]) {
-    let str = "";
-
-    products.map((item) => {
-      console.log(item.title);
-      str += item.title;
-    });
-
-    return str;
-  }
 }
