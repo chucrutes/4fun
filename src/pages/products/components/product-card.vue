c46b971210d7d7d2147c4ac87b315f0c384fd758
chucrutes <tmelo387@gmail.com>
2026-02-02 22:01:32 -0300

    feat: list expensive products

---
+++ src/pages/products/components/product-card.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Formatters } from "@/utils/formatter.utils";
+
+type ProductCardProps = {
+  name: string;
+  price: number;
+  image: string | undefined;
+  imageAlt?: string;
+};
+
+const props = defineProps<ProductCardProps>();
+</script>
+
+<template>
+  <div
+    class="border border-gray-900 rounded-2xl w-72 overflow-hidden bg-white hover:shadow-xl transition-all duration-300"
+  >
+    <div class="aspect-square w-full overflow-hidden bg-gray-100">
+      <img
+        :src="image"
+        :alt="imageAlt || name"
+        class="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
+      />
+    </div>
+
+    <div class="p-4">
+      <h3 class="text-sm text-gray-500 uppercase tracking-widest">
+        {{ name }}
+      </h3>
+      <p class="text-xl font-bold text-gray-900 mt-1">
+        R$ {{ Formatters.formatToBrazillianNumber(price) }}
+      </p>
+    </div>
+  </div>
+</template>
--- src/pages/products/dashboard/dashboard.vue
+++ src/pages/products/dashboard/dashboard.vue
@@ -6,12 +6,14 @@ import { ProductApi } from "../services/product-api";
 import { Formatters } from "@/utils/formatter.utils";
 import { ProductUtils } from "../utils/product-utils";
 import { CategoryApi } from "../services/category-api";
-import ProductTable from "../components/product.table.vue";
+import ProductTable from "../components/product-table.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import SummaryCard from "@/components/molecules/summary-card.vue";
 import { faBoxOpen, faGrip, faTag } from "@fortawesome/free-solid-svg-icons";
+import ProductCard from "../components/product-card.vue";
 
 const products = ref<Product[]>([]);
+const highestPriceProducts = ref<Product[]>([]);
 const activeCategories = ref("");
 const avgPrice = ref(0);
 const isLoading = ref(true);
@@ -25,6 +27,10 @@ onMounted(async () => {
 
     if (prodRes.success) {
       products.value = prodRes.data;
+      highestPriceProducts.value = ProductUtils.highestPriceProducts(
+        products.value,
+        5,
+      );
       avgPrice.value = ProductUtils.avgPrice(products.value);
     }
     if (catRes.success) activeCategories.value = catRes.data.length.toString();
@@ -40,15 +46,8 @@ onMounted(async () => {
       <H1>Dashboard</H1>
     </div>
     <div
-      class="gap-4 flex flex-col items-center md:flex-row md:flex-wrap md:justify-center"
+      class="gap-4 flex flex-col items-center md:flex-row md:flex-wrap md:justify-center py-4"
     >
-      <div
-        v-if="isLoading"
-        class="bg-gray-100 rounded-xl p-5 border border-gray-200 animate-pulse"
-      >
-        <div class="h-4 bg-gray-300 rounded-md w-1/2 mb-4"></div>
-        <div class="h-8 bg-gray-300 rounded-md w-1/4"></div>
-      </div>
       <SummaryCard label="Produtos" :content="products.length.toFixed(0)">
         <FontAwesomeIcon :icon="faBoxOpen" size="2xl" />
       </SummaryCard>
@@ -62,9 +61,26 @@ onMounted(async () => {
         <FontAwesomeIcon :icon="faTag" size="2xl" />
       </SummaryCard>
     </div>
+    <div class="flex items-center justify-center w-full h-24">
+      <div class="bg-gray-500 h-1 w-full"></div>
+    </div>
+    <div
+      class="gap-4 flex flex-col items-center md:flex-row md:flex-wrap md:justify-center py-4"
+    >
+      <ProductCard
+        v-for="item in highestPriceProducts"
+        :key="item.id"
+        :name="item.title"
+        :price="item.price"
+        :image="item.images[0] ?? ''"
+      />
+    </div>
+    <div class="flex items-center justify-center w-full h-24">
+      <div class="bg-gray-500 h-1 w-full"></div>
+    </div>
     <div class="py-16">
-      <div class="flex justify-center w-full overflow-x-auto">
-        <div>
+      <div class="md:flex justify-center w-full">
+        <div class="overflow-x-auto">
           <ProductTable :items="products" />
         </div>
       </div>
