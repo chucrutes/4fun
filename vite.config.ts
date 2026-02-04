afc218363fcf1ddbfc184a4dbf0e655b07bd7cd7
chucrutes <tmelo387@gmail.com>
2026-02-04 08:50:27 -0300

    chore: allow api origin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.escuelajs.co/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
