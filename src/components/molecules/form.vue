1b797de40f13c445885acfdbb306b4244d4c2e65
chucrutes <tmelo387@gmail.com>
2026-02-05 13:30:08 -0300

    fix: created component for form

---
+++ src/components/molecules/form.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type FormProps = {
+  handleSubmit: (e?: Event | undefined) => void;
+};
+
+defineProps<FormProps>();
+</script>
+
+<template>
+  <form @submit="handleSubmit" class="flex flex-col items-center gap-4">
+    <slot></slot>
+  </form>
+</template>
